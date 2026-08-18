8e8526d61f864b077716107b4e15b090cfc40e7e
Sev1l <sevil.mammadli.std@bhos.edu.az>
2026-08-18 17:31:53 +0400

    Update Lesson 2 Task 2.py

--- Lesson 2 Task 2.py
+++ Lesson 2 Task 2.py
@@ -3,9 +3,10 @@
 
 
 class Present:
-    def __init__(self,name,weight):
+    def __init__(self, name, weight):
         self.name = name
         self.weight = weight
+
     def __str__(self):
         return f'{self.name} ({self.weight} kg)'
 
@@ -14,25 +15,22 @@ class Box:
     def __init__(self):
         self.box_list = []
         self.total = 0
-    def add_present(self, present: Present):
-        self.box_list += [present]
-        self.weight = present.weight
-        
-    def total_weight(self):
-        self.total += self.weight
-        return self.total
-        
-     
-book = Present("ABC Book", 2)
 
+    def add_present(self, present: Present):
+        self.box_list.append(present)
+        self.total += present.weight
+
+    def total_weight(self):
+        return self.total
+
+
+book = Present("ABC Book", 2)
 print("The name of the present:", book.name)
 print("The weight of the present:", book.weight)
 print("Present:", book)
-
 box = Box()
 box.add_present(book)
 print(box.total_weight())
-
 cd = Present("Pink Floyd: Dark Side of the Moon", 1)
 box.add_present(cd)
 print(box.total_weight())
