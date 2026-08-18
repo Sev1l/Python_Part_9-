911a2c76f1f7d3e2295d9164c99d695245b163f3
Sev1l <sevil.mammadli.std@bhos.edu.az>
2026-08-18 17:29:15 +0400

    Update Lesson 1 Task 3.py

--- Lesson 1 Task 3.py
+++ Lesson 1 Task 3.py
@@ -2,39 +2,39 @@
 # Task 3 (Baby Centre)
 
 class Person:
-    def __init__(self,name,x,y,weight):
+    def __init__(self, name, x, y, weight):
         self.name = name
+        self.x = x
+        self.y = y
         self.weight = weight
 
 
 class BabyCentre:
-    final = 0
+    def __init__(self):
+        self.weigh_in_count = 0
+
     def weigh(self, person: Person):
         # return the weight of the person passed as an argument
-        self.final += 1
+        self.weigh_in_count += 1
         return person.weight
+
     def feed(self, person: Person):
         person.weight += 1
+
     def weigh_ins(self):
-        return(self.final)
+        return self.weigh_in_count
 
 
 baby_centre = BabyCentre()
-
 eric = Person("Eric", 1, 110, 7)
 peter = Person("Peter", 33, 176, 85)
-
 print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
-
 baby_centre.weigh(eric)
 baby_centre.weigh(eric)
-
 print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
-
 baby_centre.weigh(eric)
 baby_centre.weigh(eric)
 baby_centre.weigh(eric)
 baby_centre.weigh(eric)
-
 print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
 
