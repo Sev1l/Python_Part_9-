20d2ff274a1e380660eb552ba52698ae08843a33
Sev1l <sevil.mammadli.std@bhos.edu.az>
2026-08-18 17:30:00 +0400

    Update Lesson 1 Task 4.py

--- Lesson 1 Task 4.py
+++ Lesson 1 Task 4.py
@@ -2,6 +2,9 @@
 # Task 4 (LunchCard and PaymentTerminal)
 
 
+LUNCH_PRICE = 2.50
+SPECIAL_PRICE = 4.30
+
 
 class LunchCard:
     def __init__(self, balance: float):
@@ -16,7 +19,8 @@ class LunchCard:
             return True
         else:
             return False
-    
+
+
 class PaymentTerminal:
     def __init__(self):
         # Initially there is 1000 euros in cash available at the terminal
@@ -25,82 +29,52 @@ class PaymentTerminal:
         self.specials = 0
 
     def eat_lunch(self, payment: float):
-        if payment >= 2.50:
+        if payment >= LUNCH_PRICE:
             self.lunches += 1
-            self.funds += 2.5
-            return payment - 2.50
+            self.funds += LUNCH_PRICE
+            return payment - LUNCH_PRICE
         else:
             return payment
 
     def eat_special(self, payment: float):
-        if payment >= 4.30:
+        if payment >= SPECIAL_PRICE:
             self.specials += 1
-            self.funds += 4.30
-            return payment - 4.30
+            self.funds += SPECIAL_PRICE
+            return payment - SPECIAL_PRICE
         else:
             return payment
 
     def eat_lunch_lunchcard(self, card: LunchCard):
-        if card.balance >= 2.5:
-            card.balance -= 2.5
+        if card.balance >= LUNCH_PRICE:
+            card.balance -= LUNCH_PRICE
             self.lunches += 1
-
             return True
         else:
             return False
-
 
     def eat_special_lunchcard(self, card: LunchCard):
-        if card.balance >= 4.3:
-            card.balance -= 4.3
+        if card.balance >= SPECIAL_PRICE:
+            card.balance -= SPECIAL_PRICE
             self.specials += 1
-
             return True
         else:
             return False
+
     def deposit_money_on_card(self, card: LunchCard, amount: float):
         card.balance += amount
         self.funds += amount
 
-        
-         
 
 exactum = PaymentTerminal()
-
 card = LunchCard(2)
 print(f"Card balance is {card.balance} euros")
-
 result = exactum.eat_special_lunchcard(card)
 print("Payment successful:", result)
-
 exactum.deposit_money_on_card(card, 100)
 print(f"Card balance is {card.balance} euros")
-
 result = exactum.eat_special_lunchcard(card)
 print("Payment successful:", result)
 print(f"Card balance is {card.balance} euros")
-
 print("Funds available at the terminal:", exactum.funds)
 print("Regular lunches sold:", exactum.lunches)
 print("Special lunches sold:", exactum.specials)
-
-##exactum = PaymentTerminal()
-##
-##change = exactum.eat_lunch(10)
-##print("The change returned was", change)
-##
-##card = LunchCard(7)
-##
-##result = exactum.eat_special_lunchcard(card)
-##print("Payment successful:", result)
-##result = exactum.eat_special_lunchcard(card)
-##print("Payment successful:", result)
-##result = exactum.eat_lunch_lunchcard(card)
-##print("Payment successful:", result)
-##
-##print("Funds available at the terminal:", exactum.funds)
-##print("Regular lunches sold:", exactum.lunches)
-##print("Special lunches sold:", exactum.specials)
-
-
-
