4b5f5da31260f04a8fbf51e1209f823cb5e79d36
Sev1l <sevil.mammadli.std@bhos.edu.az>
2026-08-18 17:27:36 +0400

    Update Lesson 1 Task 1.py

--- Lesson 1 Task 1.py
+++ Lesson 1 Task 1.py
@@ -3,25 +3,22 @@
 
 
 class Car:
-    def __init__(self,make:str,top_speed:int):
+    def __init__(self, make: str, top_speed: int):
         self.make = make
         self.top_speed = top_speed
 
-def fastest_car(cars:list):
-    speed = 0
-    car = ''
-    for i in cars:
-        if i.top_speed > speed:
-            speed = i.top_speed
-            car = i.make
-    return car
-        
-    
+
+def fastest_car(cars: list):
+    return max(cars, key=lambda c: c.top_speed).make
+
+
 if __name__ == "__main__":
     car1 = Car("Saab", 195)
     car2 = Car("Lada", 110)
     car3 = Car("Ferrari", 280)
     car4 = Car("Trabant", 85)
-
     cars = [car1, car2, car3, car4]
     print(fastest_car(cars))
+
+        
+
