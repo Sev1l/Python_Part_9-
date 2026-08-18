029ad25846ccc853e7a442c64c6da8e5a6062154
Sev1l <sevil.mammadli.std@bhos.edu.az>
2026-08-18 17:34:05 +0400

    Update Lesson 3 Task 3.py

--- Lesson 3 Task 3.py
+++ Lesson 3 Task 3.py
@@ -3,36 +3,31 @@
 
 
 class WeatherStation:
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
-        self.list = []
-        self.number = 0
-    def add_observation(self,observation:str):
-        self.list += [observation]
-        self.number += 1
+        self.observations = []
+
+    def add_observation(self, observation: str):
+        self.observations.append(observation)
 
     def latest_observation(self):
-        if len(self.list) == 0:
+        if len(self.observations) == 0:
             return ''
         else:
-            return self.list[len(self.list)-1]
+            return self.observations[-1]
 
     def number_of_observations(self):
-        return self.number
+        return len(self.observations)
 
     def __str__(self):
-        return f'{self.name}, {self.number} observations'
-        
-
+        return f'{self.name}, {self.number_of_observations()} observations'
 
 
 station = WeatherStation("Houston")
 station.add_observation("Rain 10mm")
 station.add_observation("Sunny")
 print(station.latest_observation())
-
 station.add_observation("Thunderstorm")
 print(station.latest_observation())
-
 print(station.number_of_observations())
 print(station)
