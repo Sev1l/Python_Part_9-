4af636db0c6ebabbf01e2a1e308d0e1b5ccb2669
Sev1l <sevil.mammadli.std@bhos.edu.az>
2026-08-18 17:28:40 +0400

    Update Lesson 1 Task 2.py

--- Lesson 1 Task 2.py
+++ Lesson 1 Task 2.py
@@ -3,18 +3,20 @@
 
 
 class ExamSubmission:
-    def __init__(self,examinee:str,points:int):
+    def __init__(self, examinee: str, points: int):
         self.examinee = examinee
         self.points = points
+
     def __str__(self):
         return f"ExamSubmission (examinee: {self.examinee}, points: {self.points})"
 
-def passed(submissions:list ,lowest_passing:int):
-    list = []
-    for i in submissions:
-        if i.points >= lowest_passing:
-            list += [i]
-    return list    
+
+def passed(submissions: list, lowest_passing: int):
+    passing_submissions = []
+    for submission in submissions:
+        if submission.points >= lowest_passing:
+            passing_submissions.append(submission)
+    return passing_submissions
 
 
 if __name__ == "__main__":
@@ -23,7 +25,6 @@ if __name__ == "__main__":
     s3 = ExamSubmission("Paul", 15)
     s4 = ExamSubmission("Phoebe", 9)
     s5 = ExamSubmission("Persephone", 17)
-
     passes = passed([s1, s2, s3, s4, s5], 15)
     for passing in passes:
         print(passing)
